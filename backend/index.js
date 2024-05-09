69923880d9063314906f06b654846a30d74de3f5
kkartikeya22 <120785187+kkartikeya22@users.noreply.github.com>
2024-05-09 08:33:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -28,6 +29,7 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
+app.use(cors());
 
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
